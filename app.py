6bba8c2643dceb953e045161007db357d53cc52a
fa110110faizan-source <fa110110faizan@gmail.com>
2026-09-17 19:34:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import time
 import streamlit.components.v1 as components
 
 # --- MONETAG VERIFICATION CODE ---
-verification_code = ""<meta name="monetag" content="6d3578b2c52213928343828de8dc8b72">""
+verification_code = '<meta name="monetag" content="6d3578b2c52213928343828de8dc8b72">'
+
 
 components.html(verification_code, height=0, width=0)
 
